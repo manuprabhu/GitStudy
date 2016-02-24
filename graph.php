03aeeb655653966027c8615d16477e4c5bf0d812
manohar <manohar.p@iksula.com>
2016-02-24 15:38:34 +0530

    first commit

---
+++ graph.php
@@ -0,0 +1,27 @@
+<?php
+require_once('app/Mage.php')
+?>
+<script src="https://code.highcharts.com/highcharts.js"></script>
+<script src="https://code.highcharts.com/modules/exporting.js"></script>
+<div id="container" style="min-width: 310px; height: 400px; max-width: 600px; margin: 0 auto"></div>
+<script>
+$(document).ready(function(){
+    alert('asas');
+    $(function () {
+    $('#container').highcharts({
+        chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: null,
+            plotShadow: false,
+            type: 'pie'
+        },
+        title: {
+            text: 'Browser market shares January, 2015 to May, 2015'
+        },
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        },
+    });
+});
+})
+</script>
